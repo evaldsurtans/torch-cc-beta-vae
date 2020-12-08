316ba69dcdeac9993b6937e1cd90d4ab58098a4b
Evalds Urtans <evalds@asya.ai>
2020-12-08 11:09:59 +0200

    bugfix

--- main.py
+++ main.py
@@ -34,7 +34,6 @@ parser.add_argument('-batch_size', default=16, type=int)
 parser.add_argument('-learning_rate', default=1e-3, type=float)
 
 parser.add_argument('-optimizer', default='radam', type=str)
-parser.add_argument('-huber_loss_delta', default=1.0, type=float)
 
 parser.add_argument('-device', default='cuda', type=str)
 
